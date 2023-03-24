f35b028aa8d5c1529e42029845466f5e2e60de47
J0J0 <iditaroddream@gmail.com>
2023-03-24 10:50:31 -0500

    updated the about.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,18 @@
-import { ClassNames } from "@emotion/react";
+//short biography and photo
 import React from "react";
-import "./style.css";
 
 const About = () => {
   return (
-  
-    <div className="about">Hi, I am Johannah.  I am a Full Stack Devloper. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  </div>
+  <div>
+    <p className="content">Full Stack Developer</p>
+    <img className="my-pic" src={process.env.PUBLIC_URL +'/img/profile.jpg'} alt="Johannah Lear"/>
+    <p className="about">
+      Hello! I am Johannah Lear.  A current full stack student attending the University of Minnesota.
+    </p>
+    <p className="about">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    </div>
   );
 }
 
