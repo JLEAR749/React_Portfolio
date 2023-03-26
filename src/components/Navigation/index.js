a799a0ed4274d34e98fb23eb7e7dcdac0b556fd7
J0J0 <iditaroddream@gmail.com>
2023-03-26 14:49:13 -0500

    updated bug

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,43 +2,44 @@ import React from "react";
 
 const Navigation = () => {
   
-  function Navigation({ currentPage, handlePageChange }) {
-      <ul className="nav naviagtion">
-        <li className="naviagtion-item">
-          <a
-            href="#home"
-            onClick={() => handlePageChange('Home')}
-            className={currentPage === 'Home' ? 'naviagtion-link active' : 'naviagtion-link'}
-          >
-            Home
-          </a>
-        </li>
-        <li className="naviagtion-item">
-          <a
-            href="#about"
-            onClick={() => handlePageChange('About')}
-           className={currentPage === 'About' ? 'naviagtion-link active' : 'naviagtion-link'}
-          >
-            About
-          </a>
-        </li>
-        <li className="naviagtion-item">
-          <a
-            href="#project"
-            onClick={() => handlePageChange('Project')}
-            className={currentPage === 'Project' ? 'naviagtion-link active' : 'naviagtion-link'}
-          >
-            Project
-          </a>
-        </li>
-        <li className="naviagtion-item">
-          <a
-            href="#contact"
-            onClick={() => handlePageChange('Contact')}
-            className={currentPage === 'Contact' ? 'naviagtion-link active' : 'naviagtion-link'}
-          >
-            Contact
-          </a>
-        </li>
-      </ul>
-      return(<></>)
+
+      return<ul className="nav naviagtion">
+      <li className="naviagtion-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange('Home')}
+          className={currentPage === 'Home' ? 'naviagtion-link active' : 'naviagtion-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="naviagtion-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+         className={currentPage === 'About' ? 'naviagtion-link active' : 'naviagtion-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="naviagtion-item">
+        <a
+          href="#project"
+          onClick={() => handlePageChange('Project')}
+          className={currentPage === 'Project' ? 'naviagtion-link active' : 'naviagtion-link'}
+        >
+          Project
+        </a>
+      </li>
+      <li className="naviagtion-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'naviagtion-link active' : 'naviagtion-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>}
+
+  export default Navigation;
